61256f37ae581755f1f78b85e518c707e1ce4c15
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-03-25 18:51:11 +0000

    twwek some bits but not done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 David.E All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: David.E
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle () {
+  // input
+  const hoursPERWeek = parseInt(document.getElementById('Hours-per-week').value)
+  const paymentPERHour = parseInt(document.getElementById('Payment-per-hour').value)
+
+  // process
+  const areaOfTriangle = baseOfTriangle * heightOfTriangle
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+}
